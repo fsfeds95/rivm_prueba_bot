3b6f57b9e6282d2a32b686d19a181207f903fcb8
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-26 04:51:13 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -9,29 +9,55 @@ const request = require('request');
 
 const API_KEY = '74dc824830c7f93dc61b03e324070886';
 const BOT_TOKEN = '8180114783:AAFrGu06UhD3DH0wM6VYDupf177JBKz9uHI';
-
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('inline_query', async (ctx) => {
  const query = ctx.inlineQuery.query;
  if (!query) return;
 
- const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-MX&query=${encodeURIComponent(query)}`;
+ const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${encodeURIComponent(query)}`;
 
  request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
    const results = JSON.parse(body).results;
    const inlineResults = results.map(movie => ({
-    type: 'photo',
-    id: movie.id,
-    photo_url: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
-    caption: `**${movie.title} (${movie.release_date.split('-')[0]})**\n**Título original:** ${movie.original_title}\n**Idioma original:** ${movie.original_language}\n**Géneros:** ${movie.genre_ids.join(', ')}\n**Sinopsis:** ${movie.overview}`,
-    parse_mode: 'Markdown',
-    thumb_url: `https://image.tmdb.org/t/p/w92${movie.poster_path}`,
-    description: `${movie.title} (${movie.release_date.split('-')[0]})\n${movie.original_title}`,
+    type: 'article',
+    id: movie.id.toString(),
+    title: movie.title,
+    input_message_content: {
+     message_text: `${movie.title} (${movie.release_date.split('-')[0]})\n${movie.original_title}\n${movie.original_language}\n${movie.genre_ids.join(', ')}\n${movie.overview}`,
+     parse_mode: 'Markdown',
+    },
+    thumb_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+    description: movie.title,
+    reply_markup: {
+     inline_keyboard: [[
+      { text: 'Ver más', callback_data: movie.id }
+                    ]]
+    }
    }));
 
    ctx.answerInlineQuery(inlineResults);
+  }
+ });
+});
+
+bot.action(/(\d+)/, async (ctx) => {
+ const movieId = ctx.match[1];
+ const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=es-ES`;
+
+ request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+   const movie = JSON.parse(body);
+   const backdropUrl = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+   const message = `
+                *${movie.title} (${movie.release_date.split('-')[0]})*
+                _${movie.original_title}_
+                Idioma: ${movie.original_language}
+                Géneros: ${movie.genres.map(genre => genre.name).join(', ')}
+                Sinopsis: ${movie.overview}
+            `;
+   ctx.replyWithPhoto(backdropUrl, { caption: message, parse_mode: 'Markdown' });
   }
  });
 });
