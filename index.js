64532bcb0f4361e8d1e0f077edefcde3b02540f6
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-26 04:22:35 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -16,27 +16,18 @@ bot.on('inline_query', async (ctx) => {
  const query = ctx.inlineQuery.query;
  if (!query) return;
 
- const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${encodeURIComponent(query)}`;
+ const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-MX&query=${encodeURIComponent(query)}`;
 
  request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
    const results = JSON.parse(body).results;
    const inlineResults = results.map(movie => ({
-    type: 'article',
+    type: 'photo',
     id: movie.id,
-    title: movie.title,
-    input_message_content: {
-     message_text: `
-                    ![Backdrop](${movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : ''})
-                    **${movie.title} (${movie.release_date.split('-')[0]})**
-                    *Título original:* ${movie.original_title}
-                    *Idioma original:* ${movie.original_language}
-                    *Géneros:* ${movie.genre_ids.join(', ')}
-                    *Sinopsis:* ${movie.overview}
-                    `,
-    },
+    photo_url: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
     thumb_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-    description: `${movie.title} (${movie.release_date.split('-')[0]})\n${movie.original_title}`,
+    caption: `**${movie.title} (${movie.release_date.split('-')[0]})**\n**Título original:** ${movie.original_title}\n**Idioma original:** ${movie.original_language}\n**Géneros:** ${movie.genre_ids.join(', ')}\n**Sinopsis:** ${movie.overview}`,
+    parse_mode: 'Markdown',
    }));
 
    ctx.answerInlineQuery(inlineResults);
