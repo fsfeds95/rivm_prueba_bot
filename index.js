6b3435e83c1172fbbc4fd3a7a3c7dc67a9052261
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-26 12:02:53 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 8225;
 const { Telegraf } = require('telegraf');
 // Importar las bibliotecas requeridas
 const jimp = require('jimp-compact');
+const request = require('request');
 
 // el API TOKEN del bot
 const BOT_TOKEN = '8180114783:AAHQuvWCUFo98JjMK5NeAX_AQkawBy7xHec';
@@ -34,6 +35,33 @@ bot.start((ctx) => {
  }
 
  ctx.reply(`¡Hola ${firstName} bienvenido, este es tu usuario ${username}!`);
+});
+
+
+// Comando para buscar backdrops
+bot.command('backdrops', (ctx) => {
+ const idMovie = ctx.message.text.split(' ')[1]; // Obtiene el ID de la película del mensaje
+ const url = `${BASE_URL}/movie/${idMovie}/images?${API_KEY}`;
+
+ request(url, (error, response, body) => {
+  if (error) {
+   console.log('Ay, mi amor, algo salió mal:', error);
+   ctx.reply('Ocurrió un error al buscar los backdrops. Intenta de nuevo más tarde.');
+   return;
+  }
+
+  const backdrops = JSON.parse(body).backdrops;
+
+  if (backdrops.length === 0) {
+   ctx.reply('Lo siento, no se encontraron backdrops para esta película.');
+   return;
+  }
+
+  backdrops.forEach(backdrop => {
+   const backdropUrl = IMG_ORI + backdrop.file_path; // Genera la URL de cada backdrop
+   ctx.replyWithPhoto(backdropUrl); // Envía la imagen
+  });
+ });
 });
 
 
