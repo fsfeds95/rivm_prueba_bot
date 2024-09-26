f721aaa8064992b058773e173cf77a3aad22fbf5
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-26 04:07:25 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,48 +7,43 @@ const port = 8225;
 const { Telegraf } = require('telegraf');
 const request = require('request');
 
-// Definir la API key de TheMovieDB y el token del bot
 const API_KEY = '74dc824830c7f93dc61b03e324070886';
-
 const BOT_TOKEN = '8180114783:AAFrGu06UhD3DH0wM6VYDupf177JBKz9uHI';
 
-// Crear el bot
 const bot = new Telegraf(BOT_TOKEN);
 
-// Comando para buscar películas
-bot.command('pelicula', (ctx) => {
- const query = ctx.message.text.split(' ')[1]; // Obtener la película a buscar
+bot.on('inline_query', async (ctx) => {
+ const query = ctx.inlineQuery.query;
+ if (!query) return;
 
- // Hacer la solicitud a TheMovieDB
- request(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
-   const data = JSON.parse(body);
+ const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${encodeURIComponent(query)}`;
 
-   // Enviar lista de resultados al usuario
-   data.results.forEach((result, index) => {
-    ctx.reply(`${index + 1}. ${result.title} (${result.release_date.split('-')[0]})`);
-   });
+ request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+   const results = JSON.parse(body).results;
+   const inlineResults = results.map(movie => ({
+    type: 'article',
+    id: movie.id,
+    title: movie.title,
+    input_message_content: {
+     message_text: `
+                    ![Backdrop](${movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : ''})
+                    **${movie.title} (${movie.release_date.split('-')[0]})**
+                    *Título original:* ${movie.original_title}
+                    *Idioma original:* ${movie.original_language}
+                    *Géneros:* ${movie.genre_ids.join(', ')}
+                    *Sinopsis:* ${movie.overview}
+                    `,
+    },
+    thumb_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+    description: `${movie.title} (${movie.release_date.split('-')[0]})\n${movie.original_title}`,
+   }));
 
-   // Manejar la selección del usuario
-   bot.hears(/^\d+$/, (ctx) => {
-    const selectedMovie = data.results[parseInt(ctx.message.text) - 1];
-
-    // Enviar detalles de la película seleccionada
-    ctx.reply(`
-     🎬 ${selectedMovie.title} (${selectedMovie.release_date.split('-')[0]})
-     🌟 ${selectedMovie.original_title}
-     🗣️ ${selectedMovie.original_language}
-     🎭 ${selectedMovie.genres.map(genre => genre.name).join(', ')}
-     📝 ${selectedMovie.overview}
-    `);
-   });
-  } else {
-   ctx.reply('¡Ups! Hubo un error al buscar la película. Inténtalo de nuevo más tarde.');
+   ctx.answerInlineQuery(inlineResults);
   }
  });
 });
 
-// Iniciar el bot 
 bot.launch();
 
 //=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=\\
