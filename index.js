8074e046eccb33f7d1f6daa872a874e0e984b053
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-10-02 18:47:32 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ const xml2js = require('xml2js');
 const BOT_TOKEN = '7723354766:AAFlbfzZWUnQ7rAed69_yF0g2U-g2bMjAmg';
 
 const bot = new Telegraf(BOT_TOKEN);
-const RSS_URL = [
-    'https://www.cinemascomics.com/cine/feed/',
-    'https://www.cinemascomics.com/series-de-television/feed/'
-];
+
+const RSS_cine = 'https://www.cinemascomics.com/cine/feed/';
+const RSS_serie = 'https://www.cinemascomics.com/series-de-television/feed/';
 
 const extractImage = (content) => {
  const match = content.match(/<img[^>]+src="([^">]+)"/);
@@ -32,14 +31,14 @@ const isValidImageUrl = (url, callback) => {
  });
 };
 
-const fetchNews = (ctx = null) => {
- RSS_URL.forEach(url => {
+const fetchCine = (ctx = null) => {
+ RSS_cine.forEach(url => {
   request(url, (error, response, body) => {
    if (!error && response.statusCode === 200) {
     xml2js.parseString(body, (err, result) => {
      if (!err) {
       const items = result.rss.channel[0].item;
-      const randomArticles = items.sort(() => 0.5 - Math.random()).slice(0, 3); // Artículos aleatorios
+      const randomArticles = items.sort(() => 0.5 - Math.random()).slice(0, 5); // Artículos aleatorios
 
       if (ctx) {
        randomArticles.forEach(item => {
@@ -48,7 +47,7 @@ const fetchNews = (ctx = null) => {
         const description = item.description[0];
         const content = item['content:encoded'][0];
         const imageUrl = extractImage(content); // Obtener la imagen
-        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento'];
+        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
 
         // Obtener categorías como texto plano
         const categoriesText = item.category ? item.category : [];
@@ -83,7 +82,7 @@ ${finalHashtags}
         isValidImageUrl(imageUrl, (isValid) => {
          if (isValid) {
           // Crear un botón para el enlace
-          const button = [{ text: '⟨🗞️⟩ Noticia ⟨🗞️⟩', url: link }];
+          const button = [{ text: '⟨🗞️⟩ Leer más ⟨🗞️⟩', url: link }];
           ctx.replyWithPhoto(imageUrl, { caption: message, reply_markup: { inline_keyboard: [button] } })
            .catch(err => console.error('Error al enviar el mensaje:', err));
          } else {
@@ -103,11 +102,90 @@ ${finalHashtags}
  });
 };
 
-bot.start((ctx) => ctx.reply('¡Hola! Estoy aquí para traerte las últimas noticias de cine. 🎬'));
+const fetchSerie = (ctx = null) => {
+ RSS_serie.forEach(url => {
+  request(url, (error, response, body) => {
+   if (!error && response.statusCode === 200) {
+    xml2js.parseString(body, (err, result) => {
+     if (!err) {
+      const items = result.rss.channel[0].item;
+      const randomArticles = items.sort(() => 0.5 - Math.random()).slice(0, 5); // Artículos aleatorios
 
-bot.command('news', (ctx) => fetchNews(ctx)); // Enviar artículos aleatorios
+      if (ctx) {
+       randomArticles.forEach(item => {
+        const title = item.title[0];
+        const link = item.link[0];
+        const description = item.description[0];
+        const content = item['content:encoded'][0];
+        const imageUrl = extractImage(content); // Obtener la imagen
+        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
 
-setInterval(() => fetchNews(), 60000); // Mantiene el bot vivo y envía solo el último artículo
+        // Obtener categorías como texto plano
+        const categoriesText = item.category ? item.category : [];
+        const catReplace = categoriesText.join(' ').replace(/\s/g, '_'); // Reemplaza espacios por guiones bajos
+        const hashtagCat = `#` + catReplace.split('_').join(' #'); // Agrega el símbolo de hashtag
+
+        // Crear un conjunto de hashtags únicos
+        const uniqueHashtags = new Set(hashtags);
+
+        // Comparar y eliminar los que ya están en hashtags
+        hashtagCat.split(' ').forEach(cat => {
+         if (cat) {
+          uniqueHashtags.delete(cat); // Elimina si ya existe
+         }
+        });
+
+        // Unir los hashtags únicos de nuevo en una cadena
+        const finalHashtags = Array.from(uniqueHashtags).join(' ');
+
+        const message = `
+⟨📰⟩ #Noticia
+▬▬▬▬▬▬▬▬▬
+⟨🍿⟩ ${title}
+▬▬▬▬▬▬▬▬▬
+⟨💭⟩ Resumen: ${description.substring(0, 1500)}...
+▬▬▬▬▬▬▬▬▬
+${finalHashtags}
+▬▬▬▬▬▬▬▬▬
+`;
+
+        // Verificar si la URL de la imagen es válida
+        isValidImageUrl(imageUrl, (isValid) => {
+         if (isValid) {
+          // Crear un botón para el enlace
+          const button = [{ text: '⟨🗞️⟩ Leer más ⟨🗞️⟩', url: link }];
+          ctx.replyWithPhoto(imageUrl, { caption: message, reply_markup: { inline_keyboard: [button] } })
+           .catch(err => console.error('Error al enviar el mensaje:', err));
+         } else {
+          console.error('URL de imagen no válida:', imageUrl);
+         }
+        });
+       });
+      }
+     } else {
+      console.error('Error al parsear el RSS:', err);
+     }
+    });
+   } else {
+    console.error('Error al obtener el RSS:', error);
+   }
+  });
+ });
+};
+
+bot.start((ctx) => ctx.reply('¡Hola! Estoy aquí para traerte artículos de cine y series.\n\nPuedes usar el comando /cine para obtener 3 artículos de cine aleatorias.\n\nPuedes usar el comando /serie para obtener 3 artículos de series aleatorias'));
+
+// Enviar artículos aleatorios
+bot.command('cine', (ctx) => fetchCine(ctx));
+
+// Enviar artículos aleatorios
+bot.command('serie', (ctx) => fetchSerie(ctx));
+
+// Mantiene el bot vivo y envía solo el último artículo
+setInterval(() => fetchCine(), 60000);
+
+// Mantiene el bot vivo y envía solo el último artículo
+setInterval(() => fetchSerie(), 60000);
 
 bot.launch();
 
